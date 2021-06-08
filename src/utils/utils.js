2a4945941f5b72b7c8e379cbc2842de564b46ec8
Grace <frankgracey@gmil.com>
2021-06-09 00:00:51 +0100

    fixed util function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,3 @@
-export const Capitalize = function (string) {
-  return string[0].toUpperCase + string.slice(1).toLowerCase;
+export const capitalize = function (string) {
+  return string[0].toUpperCase() + string.slice(1).toLowerCase();
 };
