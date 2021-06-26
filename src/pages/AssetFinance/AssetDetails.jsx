4b753c4a3ea88beee83229a891ea20bef23cce20
Grace <frankgracey@gmil.com>
2021-06-26 13:28:25 +0100

    added view of pending and completed assets

--- src/pages/AssetFinance/AssetDetails.jsx
+++ src/pages/AssetFinance/AssetDetails.jsx
@@ -1,9 +1,28 @@
-import { Box, Text, Heading, HStack } from '@chakra-ui/layout';
 import { GiFamilyHouse, GiCash } from 'react-icons/gi';
 import { Icon } from '@chakra-ui/icons';
-import { Progress, Spacer, VStack, Flex } from '@chakra-ui/react';
+import {
+  Image,
+  Box,
+  Text,
+  Heading,
+  HStack,
+  Progress,
+  Spacer,
+  VStack,
+  Flex,
+  useDisclosure,
+  Button,
+  Drawer,
+  DrawerOverlay,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerFooter,
+} from '@chakra-ui/react';
 import Card from '../../components/common/Card';
 import { daysBetween } from '../../utils/utils';
+import { useRef } from 'react';
 
 const AssetDetail = ({ assetDetail }) => {
   const { status, due_date } = assetDetail;
@@ -29,6 +48,7 @@ const AssetDetail = ({ assetDetail }) => {
           </Text>
         </VStack>
         <Spacer />
+        <AssetDetailDrawer assetDetail={assetDetail} />
         {assetDetail.due_date && (
           <VStack align="flex-end">
             <Heading size="lg" color="abudanza.highlight">
@@ -69,3 +89,200 @@ const AssetList = ({ data }) => {
 };
 
 export default AssetList;
+
+function AssetDetailDrawer({ assetDetail }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+
+  return (
+    <>
+      <Button
+        ref={btnRef}
+        size="xs"
+        colorScheme="cyan"
+        variant="outline"
+        onClick={onOpen}
+      >
+        Details
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader color="abudanza.primary">
+            Asset Finance Details
+          </DrawerHeader>
+
+          <DrawerBody>
+            <VStack fontSize="sm" color="grey" align="start">
+              <HStack>
+                <Text>Category:</Text>
+                <Text
+                  textTransform="capitalize"
+                  fontWeight="medium"
+                  color="black"
+                  size="sm"
+                >
+                  {assetDetail.category}
+                </Text>
+              </HStack>
+              <HStack>
+                <Text>Brand:</Text>
+                <Text
+                  textTransform="capitalize"
+                  fontWeight="medium"
+                  color="black"
+                  size="sm"
+                >
+                  {assetDetail.brand}
+                </Text>
+              </HStack>
+              <HStack>
+                <Text>Model:</Text>
+                <Text
+                  textTransform="capitalize"
+                  fontWeight="medium"
+                  color="black"
+                  size="sm"
+                >
+                  {assetDetail.model}
+                </Text>
+              </HStack>
+              <HStack>
+                <Text>Cost:</Text>
+                <Text
+                  textTransform="capitalize"
+                  fontWeight="medium"
+                  color="black"
+                  size="sm"
+                >
+                  ₦{Number(assetDetail.cost).toLocaleString()}
+                </Text>
+              </HStack>
+              <HStack>
+                <Text>Amount Paid:</Text>
+                <Text
+                  textTransform="capitalize"
+                  fontWeight="medium"
+                  color="black"
+                  size="sm"
+                >
+                  ₦{Number(assetDetail.amount_paid).toLocaleString()}
+                </Text>
+              </HStack>
+              <HStack>
+                <Text color="blue.500">Vendor Name:</Text>
+                <Text
+                  textTransform="capitalize"
+                  fontWeight="medium"
+                  color="black"
+                  size="sm"
+                >
+                  {assetDetail.vendor_name}
+                </Text>
+              </HStack>
+              <HStack>
+                <Text color="blue.500">Vendor Email:</Text>
+                <Text
+                  textTransform="capitalize"
+                  fontWeight="medium"
+                  color="black"
+                  size="sm"
+                >
+                  {assetDetail.vendor_email}
+                </Text>
+              </HStack>
+              <HStack>
+                <Text color="blue.500">Vendor Phone:</Text>
+                <Text
+                  textTransform="capitalize"
+                  fontWeight="medium"
+                  color="black"
+                  size="sm"
+                >
+                  {assetDetail.vendor_phone}
+                </Text>
+              </HStack>
+              <HStack>
+                <Text color="blue.500">Vendor Address:</Text>
+                <Text
+                  textTransform="capitalize"
+                  fontWeight="medium"
+                  color="black"
+                  size="sm"
+                >
+                  {assetDetail.vendor_address}
+                </Text>
+              </HStack>
+              <HStack>
+                <Text color="blue.500">Vendor City:</Text>
+                <Text
+                  textTransform="capitalize"
+                  fontWeight="medium"
+                  color="black"
+                  size="sm"
+                >
+                  {assetDetail.vendor_city}
+                </Text>
+              </HStack>
+              <HStack>
+                <Text color="blue.500">Vendor State:</Text>
+                <Text
+                  textTransform="capitalize"
+                  fontWeight="medium"
+                  color="black"
+                  size="sm"
+                >
+                  {assetDetail.vendor_state}
+                </Text>
+              </HStack>
+              <HStack>
+                <Text color="blue.500">Activation Date:</Text>
+                <Text
+                  textTransform="capitalize"
+                  fontWeight="medium"
+                  color="black"
+                  size="sm"
+                >
+                  {new Date(assetDetail.activation_date).toLocaleDateString()}
+                </Text>
+              </HStack>
+              <HStack>
+                <Text color="blue.500">Due Date:</Text>
+                <Text
+                  textTransform="capitalize"
+                  fontWeight="medium"
+                  color="black"
+                  size="sm"
+                >
+                  {new Date(assetDetail.due_date).toLocaleDateString()}
+                </Text>
+              </HStack>
+              <Heading size="sm">Proof of Payment</Heading>
+              <Image src={assetDetail.payment_proof.url} alt="payment proof" />
+              <Heading mt="5" size="sm">
+                Proforma Invoice
+              </Heading>
+              <Image
+                src={assetDetail.proforma_invoice.url}
+                alt="proforma Invoice"
+              />
+            </VStack>
+          </DrawerBody>
+
+          <DrawerFooter>
+            {/* <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button> */}
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
--- src/pages/AssetFinance/MainTabs.jsx
+++ src/pages/AssetFinance/MainTabs.jsx
@@ -1,11 +1,34 @@
-import { Box, Text, Heading, Spinner } from '@chakra-ui/react';
+import { Text, Heading, Spinner, useToast } from '@chakra-ui/react';
 import { TabPanels, Tab, Tabs, TabList, TabPanel } from '@chakra-ui/tabs';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { HIGHLIGHT } from '../../constants/colors.json';
+import { getAssets } from '../../services/api';
 import AssetList from './AssetDetails';
 
 const MainTabs = ({ activeAssets, pendingAssets, loading }) => {
+  const toast = useToast();
   const [completedAssets, setCompletedAssets] = useState([]);
+
+  useEffect(() => {
+    getAssets('completed')
+      .then(res => {
+        setCompletedAssets(res.data.payload.assets);
+      })
+      .catch(err => {
+        const message = err.response
+          ? err.response.data.message
+          : 'error fetching assets contact admin';
+        toast({
+          title: 'Error Fetching Assets',
+          description: message,
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+          position: 'top-right',
+        });
+      });
+  }, []);
+
   return (
     <Tabs background="white" shadow="lg">
       <TabList>
